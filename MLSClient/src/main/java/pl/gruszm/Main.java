acbf37b2c000823d2f02486a00329d5bb7baed38
gruszm <mariuszgruszczynski11@gmail.com>
2024-01-28 13:05:58 +0100

    ok

--- MLSClient/src/main/java/pl/gruszm/Main.java
+++ MLSClient/src/main/java/pl/gruszm/Main.java
@@ -3,20 +3,46 @@ package pl.gruszm;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        int port; // for example: 2222
+        String username;
+
+        try
+        {
+            System.out.print("Please enter the port: ");
+            port = in.nextInt();
+
+            if (port <= 0 || port > 65535)
+            {
+                throw new InputMismatchException();
+            }
+
+            // clear the buffer
+            in.nextLine();
+
+            System.out.println("Please enter the username (md5 will be generated from it)");
+            username = in.nextLine();
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.println("The port must be a positive number not greater than 65535!");
+            in.close();
+            return;
+        }
+
         LicenseClientAPI clientAPI = new LicenseClientAPI();
 
-        clientAPI.start("127.0.0.1", 2222);
-        clientAPI.setLicence("Admin", generateKey("Admin"));
+        clientAPI.start("127.0.0.1", port);
+        clientAPI.setLicence(username, generateKey(username));
 
         clientAPI.getLicenceToken();
-
-        Scanner in = new Scanner(System.in);
 
         in.nextLine();
 
